2e7184e9a47d438b33a995218dcd101d4993806d
unknown <swyner97@gmail.com>
2022-04-06 14:37:20 -0700

    add eventlistener to start button + starting pseudocode

---
+++ questions.js
@@ -0,0 +1,114 @@
+const questions = [
+    {
+        questionText: `Complete the function sum such that it sums the 2 numbers it receives.
+        function sum(x, y) {
+  
+        _____________ }`,
+
+        answerOptions: [
+            { answerText: `return x + y`, isCorrect: true },
+            { answerText: `return x * y`, isCorrect: false },
+            { answerText: `x + y`, isCorrect: false }
+        ]
+    },
+    {
+        questionText: `Which one of these functions calculates the multiplication of x * y?`,
+
+        answerOptions: [
+            {
+                answerText: `function multiply (x, y) {
+                return x + y;
+            }`, isCorrect: false
+            },
+            {
+                answerText: `function multiply (x, y) {
+                return x * y;
+            }`, isCorrect: true
+            },
+            {
+                answerText: `function multiply(x, y) {
+                x * y;
+            }`, isCorrect: false
+            }
+        ]
+    },
+    {
+        questionText: ` Complete the function such that it returns true for passing grades and false otherwise (9 and below are failing grades).
+        function isPassing(grade) {
+            _______________
+        }`,
+        answerOptions: [
+            {
+                answerText:
+                    `if grade >= 10 {
+                    return true;
+                } else {
+                    return false;
+            }`,
+                isCorrect: false
+            },
+            {
+                answerText:
+                    `if (grade >= 10) {
+                    true;
+                } else {
+                    false;
+                }`,
+                isCorrect: false
+            },
+            {
+                answerText:
+                    `if (grade >= 10) {
+                    return true;
+                }  else {
+                    return false;
+                }`,
+                isCorrect: true
+            }
+
+        ]
+
+    },
+    {
+        questionText: `What is the correct way to define a sum variable in Javascript?`,
+        answerOptions: [
+            {
+                answerText:
+                    `let sum = 0`,
+                isCorrect: true
+            },
+            {
+                answerText:
+                    `const sum = 0`,
+                isCorrect: false
+            },
+            {
+                answerText:
+                    `var sum = 0`,
+                isCorrect: false
+            },
+        ]
+    },
+]
+// const start = document.getElementById('start');
+// start.addEventListener('click', countdown);
+
+function countdown() {
+    document.getElementById("start").addEventListener("click", countdown);
+    var timeleft = 10;
+    var downloadTimer = setInterval(function () {
+        if (timeleft <= 0) {
+            clearInterval(downloadTimer);
+            document.getElementById("countdown").innerHTML = "Finished";
+        } else {
+            document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+        }
+        timeleft -= 1;
+    }, 1000);
+
+}
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,114 +1,38 @@
-const questions = [
-    {
-        questionText: `Complete the function sum such that it sums the 2 numbers it receives.
-        function sum(x, y) {
-  
-        _____________ }`,
+let instructions = document.querySelector('#instructions');
 
-        answerOptions: [
-            { answerText: `return x + y`, isCorrect: true },
-            { answerText: `return x * y`, isCorrect: false },
-            { answerText: `x + y`, isCorrect: false }
-        ]
-    },
-    {
-        questionText: `Which one of these functions calculates the multiplication of x * y?`,
+instructions.innerHTML = `<h2>Hit the 'Start' button to test your Javascript knowledge. Try to answer the following questions within the
+given time limit. Keep in mind that any incorrect answers will result with a penalty of subtracting 10 seconds
+from your time limit. Each correct answer will result in +5 seconds.</h2>`
 
-        answerOptions: [
-            {
-                answerText: `function multiply (x, y) {
-                return x + y;
-            }`, isCorrect: false
-            },
-            {
-                answerText: `function multiply (x, y) {
-                return x * y;
-            }`, isCorrect: true
-            },
-            {
-                answerText: `function multiply(x, y) {
-                x * y;
-            }`, isCorrect: false
-            }
-        ]
-    },
-    {
-        questionText: ` Complete the function such that it returns true for passing grades and false otherwise (9 and below are failing grades).
-        function isPassing(grade) {
-            _______________
-        }`,
-        answerOptions: [
-            {
-                answerText:
-                    `if grade >= 10 {
-                    return true;
-                } else {
-                    return false;
-            }`,
-                isCorrect: false
-            },
-            {
-                answerText:
-                    `if (grade >= 10) {
-                    true;
-                } else {
-                    false;
-                }`,
-                isCorrect: false
-            },
-            {
-                answerText:
-                    `if (grade >= 10) {
-                    return true;
-                }  else {
-                    return false;
-                }`,
-                isCorrect: true
-            }
+// when user clicks 'start', instructions will go away
+let startBtn = document.getElementById('start');
 
-        ]
-
-    },
-    {
-        questionText: `What is the correct way to define a sum variable in Javascript?`,
-        answerOptions: [
-            {
-                answerText:
-                    `let sum = 0`,
-                isCorrect: true
-            },
-            {
-                answerText:
-                    `const sum = 0`,
-                isCorrect: false
-            },
-            {
-                answerText:
-                    `var sum = 0`,
-                isCorrect: false
-            },
-        ]
-    },
-]
-// const start = document.getElementById('start');
-// start.addEventListener('click', countdown);
-
-function countdown() {
-    document.getElementById("start").addEventListener("click", countdown);
-    var timeleft = 10;
-    var downloadTimer = setInterval(function () {
-        if (timeleft <= 0) {
-            clearInterval(downloadTimer);
-            document.getElementById("countdown").innerHTML = "Finished";
-        } else {
-            document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
-        }
-        timeleft -= 1;
-    }, 1000);
-
-}
+startBtn.addEventListener('click', function () {
+    let instructions = document.querySelector('#instructions');
+    instructions.innerHTML = ""
+});
 
 
+// after user clicks 'start' timer will start and countdown
 
+// if time runs out then game over => take user to scoreboard
 
+// ---------------------------------------------------------------------------------------------
 
+// after user clicks 'start' user will be prompted with a question
+
+// user will be taken to next question regardless of wrong or right answer
+
+// ---------------------------------------------------------------------------------------------
+
+// create a function so that if the user chooses a wrong answer, 10 seconds will be deducted otherwise, +5 seconds 
+
+// create a function that keeps score
+
+// ---------------------------------------------------------------------------------------------
+
+// create localstorage to keep score 
+
+// populate score on scoreboard page
+
+// ---------------------------------------------------------------------------------------------
